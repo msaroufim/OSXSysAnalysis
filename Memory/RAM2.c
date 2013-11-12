df93c22bfaabe0c098207515ac441f90c3b20bf8
Mark Saroufim <Mark@Marks-MacBook-Pro-6.local>
2013-11-11 18:11:02 -0800

    improved method for reading cache and main memory, trend is not jaggedy as expected but quite linear

--- Memory/RAM2.c
+++ Memory/RAM2.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX 100
-
+#define MAX 10000
   
 typedef unsigned long long cycles_t;
 
@@ -13,6 +12,7 @@ inline cycles_t rdtsc() {
 
 typedef struct {
   int *array;
+  long long int number;
   size_t used;
   size_t size;
 } Array;
@@ -46,39 +46,42 @@ int main() {
 //initialize array
 //array of rdstc
 Array a;
-Array RDT;
-Array RDTread;
+//Array RDT;
+//Array RDTread;
 int i;
 
 initArray(&a, 5);  // initially 5 elements
-initArray(&RDT,5);
-initArray(&RDTread,5);
+//initArray(&RDT,5);
+//initArray(&RDTread,5);
 
 
 //time writing to memory
 for (i = 0; i < MAX; i++) {
-  insertArray(&RDT,rdstc())
+  //insertArray(&RDT,rdstc());
+
   insertArray(&a, i);  // automatically resizes as necessary
+  printf("%llu\n",rdtsc());
 }
 
 
 //time reading the array
-for(i = 0; i < MAX ; i++) {
-  A[i];
-  insertArray(&RDTread,rdtsc);
-}
+//for(i = 0; i < MAX ; i++) {
+//  a[i];
+//  insertArray(&RDTread,rdtsc());
+//}
 
 //plots are simple latrenvy is average time for a single operation
 //bandwith is the number of operations we can perform in a unit time interval
 
 
-printf(" The number of used elements is%d\n", a.used);  // print number of elements
-for(i = 0; i <MAX; i++) {
-printf("The %d'ith iteration took, %llu cycles",(i,RDT[i]));
-}
+//printf(" The number of used elements is%d\n", a.used);  // print number of elements
+//for(i = 0; i <MAX; i++) {
+//printf("The th iteration write took, %llu cycles \n",RDT[i]);
+//printf("the th iteration read took %llu cycles \n",RDTread[i]);
+//}
 freeArray(&a);
-freeArray(&RDT);
-freeArray(&RDTread);
+//freeArray(&RDT);
+//freeArray(&RDTread);
 
   return 0;
 }
